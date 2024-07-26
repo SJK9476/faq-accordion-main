693a0a9659e0a7ebf4c686b4408991986e1d45e8
Samuel King <samueljamesking94@gmail.com>
2024-07-26 22:28:57 +0100

    added javascript to manage answer toggle and changing of icon

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const toggleButton = 
+const revealQuestion = document.querySelectorAll(".question");
+const answer = document.querySelectorAll(".answer");
+
+revealQuestion.forEach((question) => {
+  question.addEventListener("click", () => {
+    question.nextElementSibling.classList.toggle("visually-hidden");
+
+    if (question.children[1].src.includes("icon-plus")) {
+      question.children[1].src = "./assets/images/icon-minus.svg";
+    } else {
+      question.children[1].src = "./assets/images/icon-plus.svg";
+    }
+  });
+});
